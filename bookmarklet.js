ddb7c82da75d88da84a85b13da6fa26b178994cb
Param Thakkar <pht0104@gmail.com>
2022-02-12 16:07:31 -0500

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,12 @@
+function click(n) {
+	document.getElementsByClassName("button__StyledButton-sc-1hmy6jw-0 ibmPUd")[n].click();
+}
+
+click(0);
+setTimeout(click, 200, 1);
+setTimeout(click, 400, 0);
+setTimeout(click, 600, 0);
+setTimeout(click, 800, 0);
+setTimeout(click, 1000, 0);
+setTimeout(click, 1200, 0);
+setTimeout(click, 1400, 0);
